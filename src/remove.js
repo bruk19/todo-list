7fe2ce828837b54fc75cbd79106ee5c3fb00eeae
bruk19 <brukteshome21@gmail.com>
2022-06-03 11:31:49 +0300

    Add fix linters

--- src/remove.js
+++ src/remove.js
@@ -1,16 +1,14 @@
 const clearTask = (index, list) => {
   if (list.length === 0) return;
-const fil = list[index];
-  list.filter(item => item == fil);
+  const fil = list[index];
+  list.filter(item => item === fil);
 
   document.querySelector(`#task-${index}`).parentNode.parentNode.remove();
   localStorage.setItem('tasks', JSON.stringify(list));
-  
   if (list.length === 0) return;
 
   for (let i = index - 1; i < list.length; i += 1) {
-    const nextCheck = document.getElementById("#task-1");
-    console.log(nextCheck)
+    const nextCheck = document.getElementById('#task-1');
     /*
     console.log(nextCheck)
     nextCheck.id = `task-${i + 1}`;
